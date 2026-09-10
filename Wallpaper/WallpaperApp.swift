d1f4180ebd63ce76aa175117b0fc02d56164af19
Fatih BARAÇKILIÇ <24433692+FatihBARACKILIC@users.noreply.github.com>
2026-09-10 17:03:21 +0300

    Initial Commit

---
+++ Wallpaper/WallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperApp.swift
+//  Wallpaper
+//
+//  Created by fatih on 10.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
